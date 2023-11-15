62ec97d09bbd27567e5ea6e8f555343fe7f54c4f
Sweshik Rao Nemarukommula <142836292+SweshikN@users.noreply.github.com>
2023-11-15 17:29:51 -0600

    commmitinhg elephant

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -68,4 +68,37 @@ exports.elephant_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
     };
+
+// Handle elephant update form on PUT.
+exports.elephant_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await elephant.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.elephant_type)
+    toUpdate.elephant_type = req.body.elephant_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+// Handle Costume delete on DELETE.
+exports.costume_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Costume.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
     
