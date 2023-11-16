3dc3fae116d7ba59af8b3801bcc6c02477b1039c
Sweshik Rao Nemarukommula <142836292+SweshikN@users.noreply.github.com>
2023-11-15 19:47:51 -0600

    commit5

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -137,4 +137,16 @@ exports.elephant_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`);
         }
         };    
-    
+    // Handle a delete one view with id from query
+exports.elephant_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await elephant.findById(req.query.id)
+    res.render('elephantdelete', { title: 'elephant Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
