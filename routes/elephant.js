13666a24895daf9e52e2a3164f7f5f9d5776c54d
Sweshik Rao Nemarukommula <142836292+SweshikN@users.noreply.github.com>
2023-11-29 12:48:53 -0600

    commit all

--- routes/elephant.js
+++ routes/elephant.js
@@ -7,11 +7,21 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const elephant_controlers= require('../controllers/elephant');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET elephants */
 router.get('/', elephant_controlers.elephant_view_all_Page );
-router.get('/detail', elephant_controlers.elephant_view_one_Page);
-router.get('/create', elephant_controlers.elephant_create_Page);
-router.get('/update', elephant_controlers.elephant_update_Page);
-router.get('/delete', elephant_controlers.elephant_delete_Page);
+router.get('/detail',secured, elephant_controlers.elephant_view_one_Page);
+router.get('/create',secured, elephant_controlers.elephant_create_Page);
+router.get('/update',secured, elephant_controlers.elephant_update_Page);
+router.get('/delete',secured, elephant_controlers.elephant_delete_Page);
 
 module.exports = router;
